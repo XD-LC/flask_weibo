7eee324b334109b17496200ffba7450bbb7b34e4
Li Chong <xdulichong@gmail.com>
2019-01-27 19:54:40 +0000

    BASIC FUNCTION

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ import io
 
 from setuptools import find_packages, setup
 
-with io.open('README.rst', 'rt', encoding='utf8') as f:
+with io.open('README.md', 'r', encoding='utf8') as f:
     readme = f.read()
 
 setup(
     name='flaskr',
     version='1.0.2',
     license='BSD',
-    maintainer='Pallets team',
+    maintainer='lc',
     maintainer_email='xdulichong@gmail.com',
     description='The basic blog app learned from FLASK',
     long_description=readme,
